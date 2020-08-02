841633075ebd2ca31ffd8bacc8d507472d2c7d09
Sarath <49370420+sarath1398@users.noreply.github.com>
2020-08-02 09:34:39 +0530

    Update image_scrapper.py
    Added comments for better understanding.

--- image_scrapper.py
+++ image_scrapper.py
@@ -7,11 +7,10 @@ import requests
 import os
 
 options = webdriver.ChromeOptions()
-options.binary_location = r'C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe'
-path = r'C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\chromedriver.exe'
+options.binary_location = r'C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe' #For Example only. Use your Browser path.
+path = r'C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\chromedriver.exe' #For Example only. Use your Driver path.
 #driver = webdriver.Chrome(executable_path=path,options=options)
-#driver.get('https://www.google.com')
-#options.add_argument('--headless')
+#options.add_argument('--headless') #Remove the comment if you don't want to open the browser that's running in test mode.
 
 def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_between_interactions: int = 1):
 
@@ -19,18 +18,20 @@ def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_b
         wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(sleep_between_interactions)
 
+    #....................................Build the Search Query.............................................
     search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img".format(q=query)
     wd.get(search_url)
 
     thumbnail_results = wd.find_elements_by_css_selector("img.Q4LuWd")
     #wd.execute_script("console.log(document.getElementsByTagName('img').length)")
 
+    #Click the more images button till the page has the required number of images
     while len(thumbnail_results)<max_links_to_fetch:
         load_more_button = wd.find_element_by_css_selector(".mye4qd")
         thumbnail_results = wd.find_elements_by_css_selector("img.Q4LuWd")
         if load_more_button :
             wd.execute_script("document.querySelector('.mye4qd').click();")
-            time.sleep(1)
+            time.sleep(sleep_between_interactions)
             #driver.execute_script("console.log(document.getElementsByTagName('img').length)")
             #print(f"Length of thumbnail images are : {len(thumbnail_results)}")
         else:
@@ -42,8 +43,6 @@ def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_b
         print(f"Webpage available for downloading {max_links_to_fetch}")
     
     image_urls = set()
-    image_count = 0
-    results_start = 0
 
     while image_count < max_links_to_fetch:
         scroll_to_end(wd)
@@ -52,7 +51,7 @@ def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_b
         thumbnail_results = wd.find_elements_by_css_selector("img.Q4LuWd")
         number_results = len(thumbnail_results)
 
-        for img in thumbnail_results[results_start:max_links_to_fetch]:
+        for img in thumbnail_results[0:min(max_links_to_fetch,number_results)]:
             # try to click every thumbnail such that we can get the real image behind it
             try:
                 img.click()
@@ -71,6 +70,7 @@ def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_b
                     image_urls.discard(image_urls[len(image_urls)-rem])
                 return image_urls
 
+#Download the image from the URLs
 def persist_image(folder_path: str, url: str):
 
     try:
@@ -90,6 +90,7 @@ def persist_image(folder_path: str, url: str):
     except Exception as e:
         print(f"ERROR - Could not save {url} - {e}")
 
+#Wrapper function for downloading the images
 def search_and_download(search_term: str, driver_path: str, target_path='./images', number_images=5):
     target_folder = os.path.join(target_path, '_'.join(search_term.lower().split(' ')))
 
@@ -102,12 +103,15 @@ def search_and_download(search_term: str, driver_path: str, target_path='./image
     for elem in res:
         persist_image(target_folder, elem)
 
-search_queries = ['Person of Interest']
-number_of_images=100 #Input
+if __name__=='__main__':
+    
+    search_queries = ['Person of Interest'] # Mention your queries as a list of queries
+    #number_of_images=100
+    number_of_images=int(input("Enter the number of images to be downloaded"))
 
-if not os.path.exists('./ScrapedImages'):
-    os.mkdir('./ScrapedImages')
-os.chdir('./ScrapedImages')
-#number_of_images=int(input("Enter the number of Images Required: "))
-for query in search_queries:
-    search_and_download(query, driver_path=path,target_path=f'./{query}', number_images=number_of_images)
+    if not os.path.exists('./ScrapedImages'):
+        os.mkdir('./ScrapedImages')
+    os.chdir('./ScrapedImages')
+    #number_of_images=int(input("Enter the number of Images Required: "))
+    for query in search_queries:
+        search_and_download(query, driver_path=path,target_path=f'./{query}', number_images=number_of_images)
