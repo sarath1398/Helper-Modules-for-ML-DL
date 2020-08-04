94bf642fecfb6d742b60564e3b96942ca3406995
Sarath <49370420+sarath1398@users.noreply.github.com>
2020-08-04 09:31:49 +0530

    Update grayscale_and_resize.py
    Added Comments for better understanding

--- CNN/grayscale_and_resize.py
+++ CNN/grayscale_and_resize.py
@@ -1,18 +1,25 @@
+'''
+Note:
+        By Default the path of this code is set for Images Scraped using Image Scrapper.
+        For already existing images, change the path of the images directory . 
+'''
 import os
 from PIL import Image
 
 def grayScaleConverter(q):
 
+    #Creates a GrayScaleConverted Folder
     if not os.path.exists('./GrayScaleConverted'):
         os.mkdir('./GrayScaleConverted')
 
-    for dirs in os.listdir(os.getcwd()+'/ScrapedImages'):
+    for dirs in os.listdir(os.getcwd()+'/ScrapedImages'): #Change the directory of image dataset here.
         
-        if q.lower()==dirs.replace('_',' ').lower():
+        if q.lower()==dirs.replace('_',' ').lower(): #Remove this if statement for converting all the image in the subfolder.
 
             if not os.path.exists(f'./GrayScaleConverted/{dirs}'):
                 os.mkdir(f'./GrayScaleConverted/{dirs}')
-
+            
+            #Grayscale Conversion
             for images in os.listdir(os.getcwd()+f"/ScrapedImages/{dirs}"):
                 img = Image.open(os.getcwd()+f'/ScrapedImages/{dirs}/{images}').convert('L')
                 img.save(f'./GrayScaleConverted/{dirs}/{images}_1.jpg')
@@ -24,7 +31,7 @@ def generateThumbnail(q):
         if q.lower()==dirs.replace('_',' ').lower():
 
             for images in os.listdir(f'./GrayScaleConverted/{dirs}'):
-                file, ext = os.path.splitext(images)
+                
                 im = Image.open(f'./GrayScaleConverted/{dirs}/{images}')
                 im.thumbnail((128,128),resample=Image.ANTIALIAS)
                 file=''.join(images.split('.jpg'))
@@ -32,7 +39,7 @@ def generateThumbnail(q):
                 os.remove(f'./GrayScaleConverted/{dirs}/{images}')
 
 if __name__=="__main__":
-    queries=['Breaking Bad']
+    queries=['Breaking Bad'] 
     for q in queries:
         grayScaleConverter(q)
         generateThumbnail(q)
